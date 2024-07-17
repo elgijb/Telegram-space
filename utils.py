bee4c5a47bf261bfd0fe788e583efcb670f9cea5
elgijb <andrejelgin8@gmail.com>
2024-07-17 18:14:55 +0300

    ediy from 17.07.24 fixed a typo in the name of the function in the util.py file edit from Andrey

--- utils.py
+++ utils.py
@@ -1,7 +1,7 @@
 import os
 import requests
 
-def get_fhile_extension(url):
+def get_file_extension(url):
     file_extension = os.path.splitext(url)[1]
     return file_extension
 
@@ -11,7 +11,7 @@ def download_images(image_urls, prefix):
     for idx, url in enumerate(image_urls):
         response = requests.get(url)
         response.raise_for_status()
-        file_extension = get_fhile_extension(url)
+        file_extension = get_file_extension(url)
         file_name = f"{prefix}_image_{idx}{file_extension}"
         with open(os.path.join(folder_name, file_name), "wb") as file:
             file.write(response.content)
